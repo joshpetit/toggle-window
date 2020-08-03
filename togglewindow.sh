bafb6a1d94f2fca4420282b5c1422b92140350e6
joshpetit <joshuapetitma@yahoo.com>
2020-08-03 09:09:38 -0400

    basic functionality

--- togglewindow.sh
+++ togglewindow.sh
@@ -4,3 +4,14 @@ then
     notify-send -h int:transient:1 "Toggle Window" "Please install xdotool"
     exit
 fi
+echo TOGGLE_WINDOW_A
+
+if [[ -z "$TOGGLE_WINDOW_A" ]];
+then
+  value="$(xdotool getactivewindow)"
+  export TOGGLE_WINDOW_A=$value
+  echo "$TOGGLE_WINDOW_A"
+else
+  xdotool windowactivate "$TOGGLE_WINDOW_A"
+  echo "yoo"
+fi
