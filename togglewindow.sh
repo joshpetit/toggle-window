10aabd8af434bd1be714db96f6d55931c423ca8b
joshpetit <joshuapetitma@yahoo.com>
2020-08-03 09:22:25 -0400

    feat: reset if window doesn't exist

--- togglewindow.sh
+++ togglewindow.sh
@@ -4,14 +4,26 @@ then
     notify-send -h int:transient:1 "Toggle Window" "Please install xdotool"
     exit
 fi
-echo TOGGLE_WINDOW_A
+
+function checkID {
+  windowID=$(printf 0x%x "$TOGGLE_WINDOW_A")
+  presence=$(xwininfo -root -tree | grep "$windowID")
+  echo "$presence"
+}
 
 if [[ -z "$TOGGLE_WINDOW_A" ]];
 then
   value="$(xdotool getactivewindow)"
   export TOGGLE_WINDOW_A=$value
   echo "$TOGGLE_WINDOW_A"
+  notify-send -h int:transient:1 "Toggle Window" "Window Set"
+elif [[ $(checkID) = "" ]]
+then
+    value="$(xdotool getactivewindow)"
+    export TOGGLE_WINDOW_A=$value
+    echo "$TOGGLE_WINDOW_A"
+    notify-send -h int:transient:1 "Toggle Window" "Window Reset"
 else
   xdotool windowactivate "$TOGGLE_WINDOW_A"
-  echo "yoo"
+  echo "Window Activate Ran"
 fi
